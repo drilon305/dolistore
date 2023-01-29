b665754bab6e3430953fb02a2638c3fc0238f259
drilon305 <drilonsulejmani305@gmail.com>
2023-01-29 18:45:30 +0100

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,55 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet
+} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Navbar from "./components/navbar/Navbar"
+import Footer from "./components/footer/Footer";
+import Product from "./pages/Product/Product";
+import Products from "./pages/Products/Products";
+
+const Layout = () => {
+  return (
+<div className="app">
+    <Navbar />
+    <Outlet />
+    <Footer />
+  </div>
+  )
+  
+}
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path:"/",
+        element: <Home />
+      },
+      {
+        path:"/products/:id",
+        element: <Products />
+      },
+      {
+        path:"/product/:id",
+        element: <Product />
+      },
+
+    ]
+  },
+  
+]);
+
+function App() {
+  return (
+    <div className="App">
+       <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
