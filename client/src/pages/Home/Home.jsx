69d6e2a1b386af62ee276ee8effad57856d3aa3c
drilon305 <drilonsulejmani305@gmail.com>
2023-02-05 18:42:53 +0100

    added categories using flexbox

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
 import Slider from '../../components/Slider/Slider'
+import Categories from '../../components/categories/Categories'
 import "./Home.scss"
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <div className='home'>
       <Slider />
       <FeaturedProducts type='featured' />
+      <Categories />
       <FeaturedProducts type='trending' />
     </div>
   )
