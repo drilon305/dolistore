5c92d91963a186df380ea61ce3b33968445bee0e
drilon305 <drilonsulejmani305@gmail.com>
2023-02-15 19:45:11 +0100

    shopping cart design

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.scss"
+import "./Navbar.scss";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import SearchIcon from "@mui/icons-material/Search";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import Cart from "../Cart/Cart";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className="navbar">
       <div className="wrapper">
@@ -21,43 +24,60 @@ const Navbar = () => {
             <KeyboardArrowDownIcon />
           </div>
           <div className="item">
-            <Link className="link" to="/products/1">Women</Link>
+            <Link className="link" to="/products/1">
+              Women
+            </Link>
           </div>
           <div className="item">
-            <Link className="link" to="/products/1">Men</Link>
+            <Link className="link" to="/products/1">
+              Men
+            </Link>
           </div>
           <div className="item">
-            <Link className="link" to="/products/1">Children</Link>
+            <Link className="link" to="/products/1">
+              Children
+            </Link>
           </div>
         </div>
 
         <div className="center">
-          <Link className="link" to="/">DOLISTORE</Link>
+          <Link className="link" to="/">
+            DOLISTORE
+          </Link>
         </div>
         <div className="right">
           <div className="items">
-              <Link className="link" to="/">Homepage</Link>
-            </div>
-            <div className="item">
-              <Link className="link" to="/">About</Link>
-            </div>
-            <div className="item">
-              <Link className="link" to="/">Contact</Link>
-            </div>
-            <div className="item">
-              <Link className="link" to="/">Store</Link>
-            </div>
+            <Link className="link" to="/">
+              Homepage
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              About
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              Contact
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              Store
+            </Link>
+          </div>
           <div className="icons">
-    <SearchIcon />
-    <PersonOutlineOutlinedIcon />
-    <FavoriteBorderOutlinedIcon />
-    <div className="cartIcon">
-      <ShoppingCartOutlinedIcon />
-      <span>0</span>
-    </div>
+            <SearchIcon />
+            <PersonOutlineOutlinedIcon />
+            <FavoriteBorderOutlinedIcon />
+            <div className="cartIcon" onClick={() => setOpen(!open)}>
+              <ShoppingCartOutlinedIcon />
+              <span>0</span>
+            </div>
           </div>
         </div>
       </div>
+      {open && <Cart />}
     </div>
   );
 };
